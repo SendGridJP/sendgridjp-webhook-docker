5a368a40c10dff55646acee6843a9afe923e002a
awwa500@gmail.com <awwa500@gmail.com>
2014-06-16 13:35:13 +0900

    add sudo to script, add redirect log for node app

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#docker build --no-cache -t sendgridjp/sendgridjp-webhook-docker .
-docker build -t sendgridjp/sendgridjp-webhook-docker .
+#sudo docker build --no-cache -t sendgridjp/sendgridjp-webhook-docker .
+sudo docker build -t sendgridjp/sendgridjp-webhook-docker .
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#docker run -p 8080:80 -p 2222:22 -p 17076:7076 -i -t sendgridjp/sendgridjp-webhook-docker /usr/bin/supervisord
-#docker run -p 8080:80 -p 2222:22 -p 17076:7076 -t sendgridjp/sendgridjp-webhook-docker /usr/sbin/sshd -D &
-docker run -p 8080:80 -p 2222:22 -p 17076:7076 -i -t sendgridjp/sendgridjp-webhook-docker 
+#sudo docker run -p 8080:80 -p 2222:22 -p 17076:7076 -i -t sendgridjp/sendgridjp-webhook-docker /usr/bin/supervisord
+#sudo docker run -p 8080:80 -p 2222:22 -p 17076:7076 -t sendgridjp/sendgridjp-webhook-docker /usr/sbin/sshd -D &
+sudo docker run -p 8080:80 -p 2222:22 -p 17076:7076 -i -t sendgridjp/sendgridjp-webhook-docker 
 
